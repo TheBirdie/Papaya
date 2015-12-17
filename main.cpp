696818e12c264605d6ae47d5622c161540e61f86
Nephe <hetier.guillaume@gmail.com>
2015-12-17 10:53:17 +0100

    Set the scene as a widget inside a QMainWindow.

--- main.cpp
+++ main.cpp
@@ -1,13 +1,18 @@
 #include "Scene.h"
+#include "mainwindow.h"
 #include <qapplication.h>
 
 int main(int argc, char** argv)
 {
   QApplication application(argc,argv);
 
-  Scene viewer;
-  viewer.setWindowTitle("animation");
-  viewer.show();
+  MainWindow mainWindow;
+  mainWindow.setWindowTitle("Papaya");
+  mainWindow.show();
+
+  //Scene viewer;
+  //viewer.setWindowTitle("animation");
+  //viewer.show();
 
   return application.exec();
 }
