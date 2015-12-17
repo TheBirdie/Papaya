73db3664d7b4b093f823575e816db0191f28dc9c
Nephe <hetier.guillaume@gmail.com>
2015-12-17 11:29:53 +0100

    Added a menu and a dock

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,10 +1,14 @@
 #include "mainwindow.h"
-#include "Scene.h"
 
 MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent)
 {
     scene = new Scene();
     setCentralWidget(scene);
+
+    menu = new QMenuBar(this);
+    dock = new QDockWidget("Image List", this);
+    dock->setAllowedAreas(Qt::LeftDockWidgetArea | Qt::RightDockWidgetArea);
+    addDockWidget(Qt::RightDockWidgetArea,dock);
 }
 
 MainWindow::~MainWindow(){
