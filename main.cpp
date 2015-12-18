e735d27f6628e671f41049f2967d31424dfc49ff
GOProjectDev1 <goproject@modalimage>
2015-12-18 09:06:26 +0100

    Remove unused code / cleanup main.cpp

--- main.cpp
+++ main.cpp
@@ -8,30 +8,11 @@
 
 int main(int argc, char** argv)
 {
-
-
-    std::cout << "Init OK" << std::endl;
     QApplication application(argc,argv);
-    GLEW_OK;
 
     MainWindow mainWindow;
     mainWindow.setWindowTitle("Papaya");
     mainWindow.show();
-    // Initialise GLFW
-    if (!glfwInit())
-    {
-        std::cerr << "Unable to init GLFW. Aborting" << std::endl;
-        return 2;
-    }
-    if (glewInit() != GLEW_OK)
-    {
-        std::cerr << "Unable to init GLEW. Aborting" << std::endl;
-        return 1;
-    }
-
-    //Scene viewer;
-    //viewer.setWindowTitle("animation");
-    //viewer.show();
 
     return application.exec();
 }
